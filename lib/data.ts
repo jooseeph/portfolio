591d7ceb36bf872f43a4a64499bf853e53a5e0ab
Yusif <yusif.bayramov.yy@gmail.com>
2024-04-04 18:35:41 +0400

    fix bugs

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,6 @@ import React from 'react';
 import { CgWorkAlt } from 'react-icons/cg';
 import { FaAngular, FaReact } from 'react-icons/fa';
 import { LuGraduationCap } from 'react-icons/lu';
-import corpcommentImg from '@/public/corpcomment.png';
-import rmtdevImg from '@/public/rmtdev.png';
-import wordanalyticsImg from '@/public/wordanalytics.png';
 
 export const links = [
   {
@@ -65,21 +62,21 @@ export const projectsData = [
     description:
       'I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.',
     tags: ['React', 'Next.js', 'MongoDB', 'Tailwind', 'Prisma'],
-    imageUrl: corpcommentImg,
+    // imageUrl: ,
   },
   {
     title: 'rmtDev',
     description:
       'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
     tags: ['React', 'TypeScript', 'Next.js', 'Tailwind', 'Redux'],
-    imageUrl: rmtdevImg,
+    // imageUrl: rmtdevImg,
   },
   {
     title: 'Word Analytics',
     description:
       'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
     tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
-    imageUrl: wordanalyticsImg,
+    // imageUrl: wordanalyticsImg,
   },
 ] as const;
 
@@ -93,7 +90,6 @@ export const skillsData = [
   'Node.js',
   'Git',
   'Tailwind',
-  'MongoDB',
   'Redux',
   'Framer Motion',
 ] as const;
